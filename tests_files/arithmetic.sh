a35c8c7c5f3715d0c026e25946c56b8a50eb7b46
kim.biloni <kim.biloni@he-arc.ch>
2018-12-03 08:50:15 +0100

    arithmetic test file

---
+++ tests_files/arithmetic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+x=5
+y=10
+add=$(( x + y ))
+echo "$x + $y = $add"
+
+sub=$(( y - x))
+echo "$y - $x = $sub"
+
+mul=$(( x * y ))
+echo "$x * $y = $mul"
+
+div=$(( y / x ))
+echo "$y / $x = $div"
