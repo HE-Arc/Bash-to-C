8d6b507f239baf0a189e2e043f1cd432ad0836c9
kim.biloni <kim.biloni@he-arc.ch>
2018-12-03 10:31:50 +0100

    cleaning parser5

--- parser5.py
+++ parser5.py
@@ -1,6 +1,6 @@
 import ply.yacc as yacc
 
-from lex5 import tokens
+from lex import tokens
 import AST
 
 '''
@@ -14,42 +14,21 @@ def p_programme_statement(p):
     p[0] = AST.ProgramNode(p[1])
 
 def p_programme_recursive(p):
-    ''' programme : statement ';' programme '''
+    ''' programme : statement newline programme '''
     p[0] = AST.ProgramNode([p[1]]+p[3].children)
 
 def p_statement(p):
-    ''' statement : assignation
-        | structure '''
+    ''' statement : assignation'''
     p[0] = p[1]
-    	
-def p_statement_print(p):
-    ''' statement : PRINT expression '''
-    p[0] = AST.PrintNode(p[2])
-
-def p_structure(p):
-    ''' structure : WHILE expression '{' programme '}' '''
-    p[0] = AST.WhileNode([p[2],p[4]])
-
-def p_expression_op(p):
-    '''expression : expression ADD_OP expression
-            | expression MUL_OP expression'''
-    p[0] = AST.OpNode(p[2], [p[1], p[3]])
     	
 def p_expression_num_or_var(p):
     '''expression : NUMBER
-        | IDENTIFIER '''
+        | IDENTIFIER
+        | STRING'''
     p[0] = AST.TokenNode(p[1])
     	
-def p_expression_paren(p):
-    '''expression : '(' expression ')' '''
-    p[0] = p[2]
-    	
-def p_minus(p):
-    ''' expression : ADD_OP expression %prec UMINUS'''
-    p[0] = AST.OpNode(p[1], [p[2]])
-    	
 def p_assign(p):
-    ''' assignation : IDENTIFIER '=' expression '''
+    ''' assignation : SET_IDENTIFIER '=' expression '''
     p[0] = AST.AssignNode([AST.TokenNode(p[1]),p[3]])
 
 def p_error(p):
@@ -58,13 +37,6 @@ def p_error(p):
         yacc.errok()
     else:
         print ("Sytax error: unexpected end of file!")
-
-
-precedence = (
-    ('left', 'ADD_OP'),
-    ('left', 'MUL_OP'),
-    ('right', 'UMINUS'),  
-)
 
 def parse(program):
     return yacc.parse(program)
@@ -75,7 +47,7 @@ if __name__ == "__main__":
     import sys 
     	
     prog = open(sys.argv[1]).read()
-    result = yacc.parse(prog)
+    result = yacc.parse(prog, debug=True)
     if result:
         print (result)
             
