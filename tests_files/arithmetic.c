70b4ce67ae425fe361b46dd926c950ddfe722a0a
malik <malik.fleury@he-arc.ch>
2019-01-03 16:21:05 +0100

    Add last C example generated

--- tests_files/arithmetic.c
+++ tests_files/arithmetic.c
@@ -6,11 +6,11 @@ int main()
 	int y = 10;
 	int z = x;
 	float add = x + y;
-	printf("$x + $y = $add");
+	printf("%d + %d = %d",5,10,x + y);
 	float sub = y - x;
-	printf("$y - $x = $sub");
+	printf("%d - %d = %d",10,5,y - x);
 	float mul = y * x;
-	printf("$x * $y = $mul");
+	printf("%d * %d = %d",5,10,y * x);
 	float div = y / x;
-	printf("$y / $x = $div");
+	printf("%d / %d = %d",10,5,y / x);
 }
